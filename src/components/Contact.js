14de747073bc3241eef7f13fe31c14ea0a74bdc6
febri4n <solihinfebrian0@gmail.com>
2022-08-16 21:51:24 +0700

    kembali ke cara penggunaan props sebelumnya

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,20 +3,20 @@ import '../index.css'
 import phoneIcon from '../images/phone-icon.png'
 import mailIcon from '../images/mail-icon.png'
 
-const Contact = ({ img, name, phone, email }) => {
-  // console.log(props);
+const Contact = (props) => {
+  console.log(props);
 
   return (
     <div className="contact-card">
-      <img src={img} alt="gambar koceng" />
-      <h3>{name}</h3>
+      <img src={props.img} alt="gambar koceng" />
+      <h3>{props.name}</h3>
       <div className="info-group">
         <img src={phoneIcon} alt="gambar hengpon" />
-        <p>{phone}</p>
+        <p>{props.phone}</p>
       </div>
       <div className="info-group">
         <img src={mailIcon} alt="gambar email" />
-        <p>{email}</p>
+        <p>{props.email}</p>
       </div>
     </div>
   )
